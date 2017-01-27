8c9b1d8f24f91f1d90b38532c484d7293b8c0d5c
Harvey-Sun <suntaocq0310@gmail.com>
2017-01-27 23:18:02 +0800

    名字更换，最终版

--- DonchianChannel.py
+++ DonchianChannel.py
@@ -6,62 +6,53 @@ import pandas as pd
 
 username = 'Harvey_Sun'
 password = 'P948894dgmcsy'
-Strategy_Name = 'DonchianChannel'
+Strategy_Name = 'DonchianChannel_parity'
 
 INIT_CAP = 100000000
 START_DATE = '20130101'
 END_DATE = '20161231'
 Fee_Rate = 0.001
+program_path = 'C:/cStrategy/'
 
 
 def initial(sdk):
     # 准备数据
     sdk.prepareData(['LZ_GPA_QUOTE_THIGH', 'LZ_GPA_QUOTE_TLOW',
                      'LZ_GPA_INDEX_CSI500MEMBER', 'LZ_GPA_SLCIND_STOP_FLAG'])
-    # 下面获取close数据是为了判断某些股票当天是否已退市
-    close = pd.read_csv('C:\cStrategy\Factor\LZ_GPA_QUOTE_TCLOSE.csv', index_col=0)
-    close.index = [str(i) for i in close.index]
-    sdk.setGlobal('close', close)
-    # 获取股票代码
-    stock_list = sdk.getStockList()
-    sdk.setGlobal('stock_list', stock_list)
-    # 找到中证500成分股
-    in_zz500 = pd.Series(sdk.getFieldData('LZ_GPA_INDEX_CSI500MEMBER')[-1]) == 1
-    sdk.setGlobal('in_zz500', in_zz500)
-    zz500 = list(pd.Series(stock_list)[in_zz500])
-    sdk.setGlobal('zz500', zz500)
-    # 建立一个字典来管理每只股票的仓位信息
-    cash_for_each_stock = INIT_CAP / 500
-    position_of_stock = {}
-    for stock in zz500:
-        position_of_stock[stock] = {}
-        position_of_stock[stock]['position'] = 0
-        position_of_stock[stock]['cash'] = cash_for_each_stock
-        # position_of_stock[stock]['last_trade_day'] = str(int(START_DATE) - 1)
-    sdk.setGlobal('position_of_stock', position_of_stock)
 
 
 def init_per_day(sdk):
-    close = sdk.getGlobal('close')
     today = sdk.getNowDate()
-    # 选出当天可交易的中证500成分股
-
-    # in_zz500 = bool(sdk.getFieldData('LZ_GPA_INDEX_CSI500MEMBER')[-1])
-    stock_list = sdk.getGlobal('stock_list')
-    in_zz500 = sdk.getGlobal('in_zz500')
+    sdk.sdklog(today, '========================================日期')
+    # 获取当天中证500成分股
+    in_zz500 = pd.Series(sdk.getFieldData('LZ_GPA_INDEX_CSI500MEMBER')[-1]) == 1
+    stock_list = sdk.getStockList()
+    zz500 = list(pd.Series(stock_list)[in_zz500])
+    sdk.setGlobal('zz500', zz500)
+    # 获取仓位信息
+    positions = sdk.getPositions()
+    sdk.sdklog(len(positions), '持有股票数量')
+    stock_with_position = [i.code for i in positions]
+    # 找到中证500外的有仓位的股票
+    out_zz500_stock = list(set(stock_with_position) - set(zz500))
+    # 以下代码获取当天未停牌未退市的股票，即可交易股票
     # not_stop = pd.isnull(sdk.getFieldData('LZ_GPA_SLCIND_STOP_FLAG')[-1])  # 当日没有停牌的股票
     not_stop = pd.isnull(sdk.getFieldData('LZ_GPA_SLCIND_STOP_FLAG')[-11:]).all(axis=0)  # 当日和前10日均没有停牌的股票
-    on_list = pd.notnull(close.ix[today])  # 存在于市场上的股票
-    zz500_not_stop = list(pd.Series(stock_list)[np.logical_and(np.logical_and(in_zz500, not_stop), on_list)])
-    sdk.setGlobal('zz500_not_stop', zz500_not_stop)
-    sdk.subscribeQuote(zz500_not_stop)
-    # 找到中证500成分股前10天的最高价和前5天的最低价
-    high = pd.DataFrame(sdk.getFieldData('LZ_GPA_QUOTE_THIGH')[-10:], columns=stock_list)[zz500_not_stop]
-    low = pd.DataFrame(sdk.getFieldData('LZ_GPA_QUOTE_TLOW')[-5:], columns=stock_list)[zz500_not_stop]
+    zz500_available = list(pd.Series(stock_list)[np.logical_and(in_zz500, not_stop)])
+    sdk.setGlobal('zz500_available', zz500_available)
+    # 以下代码获取当天被移出中证500的有仓位的股票中可交易的股票
+    out_zz500_available = list(set(pd.Series(stock_list)[not_stop]).intersection(set(out_zz500_stock)))
+    sdk.setGlobal('out_zz500_available', out_zz500_available)
+    # 订阅所有可交易的股票
+    stock_available = list(set(zz500_available + out_zz500_available))
+    sdk.sdklog(len(stock_available), '订阅股票数量')
+    sdk.subscribeQuote(stock_available)
+    # 找到所有可交易股票前10天的最高价和前5天的最低价
+    high = pd.DataFrame(sdk.getFieldData('LZ_GPA_QUOTE_THIGH')[-10:], columns=stock_list)[stock_available]
+    low = pd.DataFrame(sdk.getFieldData('LZ_GPA_QUOTE_TLOW')[-5:], columns=stock_list)[stock_available]
     up_line = high.max(axis=0)
     down_line = low.min(axis=0)
-    # 记录
-    sdk.sdklog(today, '==========================日期')
+    # 全局变量
     sdk.setGlobal('up_line', up_line)
     sdk.setGlobal('down_line', down_line)
     # 建立一个列表，来记录当天有过交易的股票
@@ -72,55 +63,79 @@ def init_per_day(sdk):
 def strategy(sdk):
     if (sdk.getNowTime() >= '093000') & (sdk.getNowTime() < '150000'):
         # today = sdk.getNowDate()
-        zz500_not_stop = sdk.getGlobal('zz500_not_stop')
+        # 每分钟开始时，加载今日已交易股票、上下轨
         traded_stock = sdk.getGlobal('traded_stock')
         up_line = sdk.getGlobal('up_line')
         down_line = sdk.getGlobal('down_line')
-        # 获取当天仓位信息
-        position_of_stock = sdk.getGlobal('position_of_stock')
-        # 获得当日还可以买入或卖出的股票代码
-        available_stock = list(set(zz500_not_stop) - set(traded_stock))
-        quotes = sdk.getQuotes(available_stock)
+        # 获取仓位信息及有仓位的股票
+        positions = sdk.getPositions()
+        position_dict = dict([[i.code, i.optPosition] for i in positions])
+        stock_with_position = [i.code for i in positions]
+        number = len(stock_with_position)
+        # 找到中证500外的有仓位的股票
+        zz500 = sdk.getGlobal('zz500')
+        out_zz500_stock = list(set(stock_with_position) - set(zz500))
+        out_num = len(out_zz500_stock)
+        # 找到目前有仓位且可交易的中证500外的股票
+        out_zz500_available = sdk.getGlobal('out_zz500_available')
+        out_zz500_tradable = list(set(out_zz500_stock).intersection(set(out_zz500_available)))
+        # 获得中证500当日可交易的股票
+        zz500_available = sdk.getGlobal('zz500_available')
+        # 去除今天已经有交易的股票，获得当下还可交易的股票
+        zz500_tradable = list(set(zz500_available) - set(traded_stock))
+        # 取得盘口数据
+        quotes = sdk.getQuotes(zz500_tradable + out_zz500_tradable)
+
+        # 考虑被移出中证500的那些股票
+        sell_orders_out500 = []
+        if out_zz500_tradable:
+            for stock in out_zz500_tradable:
+                position = position_dict[stock]
+                current_price = quotes[stock].current
+                down = down_line[stock]
+                if current_price < down:  # 判断是否卖出
+                    order = [stock, current_price, position, -1]
+                    sell_orders_out500.append(order)
+        sdk.makeOrders(sell_orders_out500)
+        # 考虑当日中证500可交易的股票
         buy_orders = []
         sell_orders = []
-        for stock in available_stock:
-            cash = position_of_stock[stock]['cash']
-            position = position_of_stock[stock]['position']
+        for stock in zz500_tradable:
+            available_cash = sdk.getAccountInfo().availableCash / (500 - number)
+            # 如果当时买入股票超过了500-number?
             current_price = quotes[stock].current
             up = up_line[stock]
             down = down_line[stock]
-            if current_price > up:
-                volume = 100 * np.floor(cash / (100 * current_price))
-                order = [stock, current_price, volume, 1]
-                buy_orders.append(order)
-                traded_stock.append(stock)  # 这里有待考虑，下单后不一定会成交,现假设都能成交
-                # 计算交易后的仓位变化，并记录在position_of_stock中
-                cash_payed = volume * current_price * (1 + Fee_Rate)
-                position_of_stock[stock]['cash'] = cash - cash_payed
-                position_of_stock[stock]['position'] = position + volume
-                # position_of_stock[stock]['last_trade_day'] = today
-            elif (current_price < down) & (position > 0):
-                volume = position
+            if (current_price > up) & (stock not in stock_with_position):
+                volume = 100 * np.floor(available_cash / (100 * current_price))
+                if volume > 0:
+                    order = [stock, current_price, volume, 1]
+                    buy_orders.append(order)
+                    traded_stock.append(stock)  # 这里有待考虑，下单后不一定会成交,现假设都能成交
+            elif (current_price < down) & (stock in stock_with_position):
+                volume = position_dict[stock]
                 order = [stock, current_price, volume, -1]
                 sell_orders.append(order)
                 traded_stock.append(stock)
-                # 计算交易后的仓位变化，并记录在position_of_stock中
-                cash_received = volume * current_price * (1 - Fee_Rate)
-                position_of_stock[stock]['cash'] = cash + cash_received
-                position_of_stock[stock]['position'] = 0
-                # position_of_stock[stock]['last_trade_day'] = today
             else:
                 pass
-        sdk.makeOrders(buy_orders)
         sdk.makeOrders(sell_orders)
-        if buy_orders or sell_orders:
+        sdk.makeOrders(buy_orders)
+        # 记录下单数据
+        if buy_orders or sell_orders or sell_orders_out500:
             sdk.sdklog(sdk.getNowTime(), '=================时间')
-            sdk.sdklog('Buy orders')
-            sdk.sdklog(np.array(buy_orders))
-            sdk.sdklog('Sell orders')
-            sdk.sdklog(np.array(sell_orders))
+            if buy_orders:
+                sdk.sdklog('Buy orders')
+                sdk.sdklog(np.array(buy_orders))
+            if sell_orders:
+                sdk.sdklog('Sell orders')
+                sdk.sdklog(np.array(sell_orders))
+            if sell_orders_out500:
+                sdk.sdklog('Sell removed stocks')
+                sdk.sdklog(np.array(sell_orders_out500))
+
         sdk.setGlobal('traded_stock', traded_stock)
-        sdk.setGlobal('position_of_stock', position_of_stock)
+
 
 config = {
     'username': username,
@@ -134,11 +149,11 @@ config = {
     'feeRate': Fee_Rate,
     'strategyName': Strategy_Name,
     'logfile': '%s.log' % Strategy_Name,
-    'rootpath': 'C:/cStrategy/',
+    'rootpath': program_path,
     'executeMode': 'M',
     'feeLimit': 5,
     'cycle': 1,
-    'playBackTime': 1,
+    'dealByVolume': True,
     'allowfortodayfactor': ['LZ_GPA_INDEX_CSI500MEMBER', 'LZ_GPA_SLCIND_STOP_FLAG']
 }
 
